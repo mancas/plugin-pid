cb342b85424e60a83865e61a000960c873da7030
mancas <Manu@Manu-PC>
2014-01-05 17:10:13 +0100

    Hminima extend with otsu and huang

--- plugin-pid/src/NeuromuscularDisease_Plugin.java
+++ plugin-pid/src/NeuromuscularDisease_Plugin.java
@@ -25,6 +25,7 @@ public class NeuromuscularDisease_Plugin implements PlugInFilter, ActionListener
 	
 	private ImagePlus imp;
 	private ImagePlus greenChannel;
+	private ImagePlus redChannel;
 	private Integer averageIntensityGreen;
 	private JFrame tutorialFrame;
 	private JTextArea descriptionField;
@@ -32,11 +33,14 @@ public class NeuromuscularDisease_Plugin implements PlugInFilter, ActionListener
 
 	public void run(ImageProcessor ip) {
 		//We need to check if the current image is RGB or not.
-		if (imp.getBitDepth() != 64) {
+		if (ip.isGrayscale()) {
 			IJ.showMessage("Neuromuscular Disease", "Debe seleccionar una imagen RGB.");
+			return;
 		}
-		this.greenChannel = this.getGreenChannel(ip);		
+		this.greenChannel = this.getGreenChannel(ip);
+		this.redChannel = this.getRedChannel(ip);	
 		this.greenChannel.show();
+		this.redChannel.show();
 		this.createTutorialFrame();
 		this.tutorialFrame.setVisible(true);
 	}
@@ -75,6 +79,30 @@ public class NeuromuscularDisease_Plugin implements PlugInFilter, ActionListener
 	}
 	
 	/*
+	 * @param ImageProcessor
+	 * @return ImagePlus
+	 */
+	private ImagePlus getRedChannel(ImageProcessor ip)
+	{
+		ColorProcessor cp = (ColorProcessor) ip;
+		int width = ip.getWidth();
+		int height = ip.getHeight();
+		byte[] red = new byte[width*height];
+		//Extract the red channel (number 1)
+		red = cp.getChannel(1);
+		ImagePlus redImage = NewImage.createByteImage("Red_Channel", width, height, 1, NewImage.FILL_WHITE);
+
+		byte[] pixels = (byte[]) redImage.getProcessor().getPixels();
+		
+		//Copy green channel to the new image
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = red[i];
+		}
+
+		return redImage;
+	}
+	
+	/*
 	 * @param ImagePlus
 	 * @return Integer
 	 */
@@ -98,12 +126,17 @@ public class NeuromuscularDisease_Plugin implements PlugInFilter, ActionListener
 	
 	private void hMinima(ImagePlus ip, int h)
 	{
+		IJ.run(this.greenChannel, "Enhance Contrast...", "saturated=0.4 equalize");
+		//First thing we must to do is duplicate the main image
 		ImagePlus _b = ip.duplicate();
 		_b.setTitle("_b");
 		_b.show();
 		IJ.selectWindow("_b");
+
 		IJ.run("Subtract...", "value="+h);
 		IJ.run("GreyscaleReconstruct ", "mask=Green_Channel seed=_b create");
+		
+		
 		ImagePlus reconstructed = WindowManager.getImage("Reconstructed");
 		IJ.selectWindow("Reconstructed");
 		reconstructed.setTitle("hMinima transform");
@@ -111,14 +144,103 @@ public class NeuromuscularDisease_Plugin implements PlugInFilter, ActionListener
 		//These lines allow us to close the temp img without been saved
 		_b.changes = false;
 		_b.close();
+		
+
 		//IJ.run("Domes ", "height=1 basins");
 		//IJ.setThreshold(1, 255);
 		//IJ.run("Convert to Mask");
-		//IJ.run("Threshold", "method=Percentile mode=B&W");
+
 		//Set threshold
-		IJ.setAutoThreshold(reconstructed, "Percentile");
+		IJ.setAutoThreshold(reconstructed, "Huang");
 		Prefs.blackBackground = false;
 		IJ.run(reconstructed, "Convert to Mask", "");
+		this.sumSobel(reconstructed);
+		IJ.setAutoThreshold(reconstructed, "Otsu");
+		Prefs.blackBackground = false;
+		IJ.run(reconstructed, "Convert to Mask", "");
+	}
+	
+	/*
+	 * This method enhance the image by applying some dilates and erodes
+	 */
+	private void imageEnhancement(ImagePlus ip)
+	{
+		IJ.selectWindow("hMinima transform");
+		IJ.run("BinaryDilate ", "coefficient=2 iterations="+4+" white");
+		IJ.run("BinaryErode ", "coefficient=2 iterations="+4+" white");
+		IJ.run("BinaryDilate ", "coefficient=2 iterations="+4+" black");
+		IJ.run("BinaryErode ", "coefficient=2 iterations="+4+" black");
+		IJ.run("BinaryDilate ", "coefficient=2 iterations="+4+" white");
+		IJ.run("BinaryErode ", "coefficient=2 iterations="+4+" white");
+		IJ.run("BinaryDilate ", "coefficient=2 iterations="+4+" black");
+		IJ.run("BinaryErode ", "coefficient=2 iterations="+4+" black");
+	}
+	
+	/*
+	 * This method apply sobel masks to the image pass through parameter
+	 */
+	private void sobel(ImagePlus ip)
+	{
+		int w = ip.getWidth();
+        int h = ip.getHeight();
+        //Sobel Mask
+        int sobel_x[][] = {{-1,0,1},
+                {-2,0,2},
+                {-1,0,1}};
+		int sobel_y[][] = {{-1,-2,-1},
+		                {0,0,0},
+		                {1,2,1}};
+		int pixel_x;
+		int pixel_y;
+		ImageProcessor processor = ip.getProcessor();
+        ImageProcessor copy = processor.duplicate();
+
+        for (int x=1; x < w-2; x++) {
+            for (int y=1; y < h-2; y++) {
+                pixel_x = (sobel_x[0][0] * copy.getPixel(x-1,y-1)) + (sobel_x[0][1] * copy.getPixel(x,y-1)) + (sobel_x[0][2] * copy.getPixel(x+1,y-1)) +
+                    (sobel_x[1][0] * copy.getPixel(x-1,y))   + (sobel_x[1][1] * copy.getPixel(x,y))   + (sobel_x[1][2] * copy.getPixel(x+1,y)) +
+                    (sobel_x[2][0] * copy.getPixel(x-1,y+1)) + (sobel_x[2][1] * copy.getPixel(x,y+1)) + (sobel_x[2][2] * copy.getPixel(x+1,y+1));
+
+                pixel_y = (sobel_y[0][0] * copy.getPixel(x-1,y-1)) + (sobel_y[0][1] * copy.getPixel(x,y-1)) + (sobel_y[0][2] * copy.getPixel(x+1,y-1)) +
+                    (sobel_y[1][0] * copy.getPixel(x-1,y))   + (sobel_y[1][1] * copy.getPixel(x,y))   + (sobel_y[1][2] * copy.getPixel(x+1,y)) +
+                    (sobel_y[2][0] * copy.getPixel(x-1,y+1)) + (sobel_y[2][1] * copy.getPixel(x,y+1)) + (sobel_x[2][2] * copy.getPixel(x+1,y+1));
+
+                int val = (int)Math.sqrt((pixel_x * pixel_x) + (pixel_y * pixel_y));
+
+                if(val < 0)
+                {
+                   val = 0;
+                }
+
+                if(val > 255)
+                {
+                   val = 255;
+                }
+
+                processor.putPixel(x,y,val);
+            }
+        }
+	}
+	
+	/*
+	 * This method sum the original image and the result obtained from applying sobel masks
+	 */
+	private void sumSobel(ImagePlus ip)
+	{
+		int w = ip.getWidth();
+		int he = ip.getHeight();
+		ImageProcessor original = ip.getProcessor();
+		ImagePlus copy = this.greenChannel;
+		ImageProcessor copy_processor = copy.getProcessor();
+		this.sobel(copy);
+		
+		for (int i = 0; i < w; i++) {
+			for (int j = 0; j < he; j++) {
+				original.putPixel(i, j, (original.getPixel(i, j) + copy_processor.getPixel(i, j)));
+			}
+		}
+		copy.changes = false;
+		copy.close();
 	}
 	
 	/*
@@ -142,7 +264,7 @@ public class NeuromuscularDisease_Plugin implements PlugInFilter, ActionListener
 		this.descriptionField.setLineWrap(true);
 		
 		JScrollPane scroller = new JScrollPane(this.descriptionField);
-		scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+		scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
 		scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 	
 		
@@ -159,7 +281,7 @@ public class NeuromuscularDisease_Plugin implements PlugInFilter, ActionListener
 		
 		frame.getContentPane().add(BorderLayout.CENTER, topPanel);
 		frame.pack();
-		frame.setResizable(false);
+		frame.setResizable(true);
 		this.tutorialFrame = frame;
 	}
 
@@ -167,9 +289,24 @@ public class NeuromuscularDisease_Plugin implements PlugInFilter, ActionListener
 		this.count++;
 		switch (this.count) {
 		case 1:
-			this.descriptionField.setText("Una vez obtenida la matriz del canal verde, aplicamos el algoritmo H-Minima extendido para obtener la separación de las células.");
+			this.descriptionField.setText("Una vez obtenida la matriz" +
+					" del canal verde, aplicamos el algoritmo H-Minima," + 
+					" con un valor de h igual a 2/3 de la intensidad media del canal verde," + 
+					" para obtener la separación de las células. Después se aplica una umbralización" + 
+					" utilizando el algoritmo Huang puesto que este es el que mejores resultados obtiene. Tras esto," +
+					" se le aplica las máscaras de Sobel a la imagen resultante y se vuelve a umbralizar con el algoritmo Otsu.");
+			this.tutorialFrame.repaint();
 			Integer h = this.getAverageIntensity(this.greenChannel);
-			this.hMinima(this.greenChannel, (2*h)/3);
+			this.hMinima(this.greenChannel, (h)/2);
+			break;
+		case 2:
+			this.descriptionField.setText("Tras la aplicación del algoritmo H-Minima, debemos mejorar la imagen" +
+					" para reducir al máximo ruido, pequeñas discontinuidades etc." + 
+					" Con este objetivo, aplicamos una serie de dilataciones y erorsiones utilizando el mismo elemento estructural." + 
+					" Al utilizar el mismo elemento estructural para realizar una dilatación y a continuación una erosión," + 
+					" se consigue tener la misma imagen salvo aquellas pequeñas discontinuidades que cubra el elemento estructural utilizado.");
+			this.tutorialFrame.repaint();
+			this.imageEnhancement(this.greenChannel);
 			break;
 		}
 		
